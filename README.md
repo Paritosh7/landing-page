16b2782983acb8c12e42f2fa3467c67bc19ecdc6
Paritosh <paritosh@Paritoshs-MacBook-Pro.local>
2020-09-29 09:40:12 +0530

    scrollIntoView | responsiveness across platform fixes

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 
 ## Table of Contents
 
-* [About](#about)
+- [About](#about)
 
 ## About
 
-The project is a small template for a landing page. It has got peculiar features of animation, where required, with smooth *scrolling*. Floating button which can be used to scroll to top is another feature implemented. 
+The project is a small template for a landing page. It has got peculiar features of animation, where required, with smooth _scrolling_. Floating button which can be used to scroll to top is another feature implemented.
--- js/app.js
+++ js/app.js
@@ -1,48 +1,47 @@
 /**
- * 
+ *
  * Manipulating the DOM exercise.
  * Exercise programmatically builds navigation,
  * scrolls to anchors from navigation,
  * and highlights section in viewport upon scrolling.
- * 
+ *
  * Dependencies: None
- * 
+ *
  * JS Version: ES2015/ES6
- * 
+ *
  * JS Standard: ESlint
- * 
-*/
+ *
+ */
 
-/** Initialising global variables 
+/** Initialising global variables
  * const mainContentSections -> provides us a live html collection
  * listItemArray -> for keeping a track the items in the navbar
  * listDocumentFragment -> for lightwight addition of navbar items to the                             navbar
  * topBtn -> to access the floating btn
  */
-const mainContentSections = document.getElementsByTagName('section');
+const mainContentSections = document.getElementsByTagName("section");
 let listItemArray = [];
 let listDocumentFragment = document.createDocumentFragment();
-const topBtn = document.querySelector('#top');
+const topBtn = document.querySelector("#top");
 
-console.log(window.innerHeight)
-console.log(document.documentElement.clientHeight)
+console.log(window.innerHeight);
+console.log(document.documentElement.clientHeight);
 
 /** programmatic initialisation of the navbar. */
 function initialiseNavbar() {
   for (let section of mainContentSections) {
-      createNavbarListItem(section);
+    createNavbarListItem(section);
   }
 }
 
 /** creating navbar items based on sections. */
-function createNavbarListItem(section){
-
-  const li = document.createElement('li');
-  const a = document.createElement('a');
+function createNavbarListItem(section) {
+  const li = document.createElement("li");
+  const a = document.createElement("a");
 
   a.textContent = section.dataset.nav;
   a.classList.add("menu__link");
-  a.setAttribute('href',"#"+section.getAttribute('id'))
+  a.setAttribute("data-flow", section.getAttribute("id"));
 
   listItemArray.push(a);
 
@@ -51,15 +50,24 @@ function createNavbarListItem(section){
   addNavbarListItemToFragment(li);
 }
 
+/** using scrollIntoView to show element when clicked on anchor
+ * tag, without using href.
+ */
+document.addEventListener("click", (eve) => {
+  if (!eve.target.matches(".menu__link")) return;
+  eve.preventDefault();
+  let element = document.getElementById(eve.target.dataset.flow);
+  element.scrollIntoView();
+});
+
 /** adding list in the navbar based on
  * the sections present at a given time.
  */
-function addNavbarListItemToFragment(li){
+function addNavbarListItemToFragment(li) {
   listDocumentFragment.appendChild(li);
 }
 
-
-/** calling initialiseNavbar() the first 
+/** calling initialiseNavbar() the first
  * time to initialise the navbar.
  */
 initialiseNavbar();
@@ -69,95 +77,92 @@ initialiseNavbar();
  */
 appendFragmentToNavbar();
 
-
 /** to add items based on the user requirement.  */
-document.querySelector('.add-icon').addEventListener('click',function(eve) {
-  
+document.querySelector(".add-icon").addEventListener("click", function (eve) {
   addSection(eve);
 
-  appendFragmentToNavbar()
-
-})
+  appendFragmentToNavbar();
+});
 
 /** adding section before the end of the current main#main_content,
  * then adding creating navbar item for the same section
  */
-function addSection(eve){
-
-  document.getElementById('main__content').insertAdjacentHTML("beforeend",returnSectionString())
+function addSection(eve) {
+  document
+    .getElementById("main__content")
+    .insertAdjacentHTML("beforeend", returnSectionString());
 
   createNavbarListItem(mainContentSections[listItemArray.length]);
-
 }
 
-
 /** appending fragment to the navbar */
-function appendFragmentToNavbar(){
-  let navbarList = document.getElementById('navbar__list');
-  console.log(listDocumentFragment.children)
+function appendFragmentToNavbar() {
+  let navbarList = document.getElementById("navbar__list");
+  console.log(listDocumentFragment.children);
   navbarList.appendChild(listDocumentFragment);
 }
 
-/** method to check the scrolling state for a section, 
+/** method to check the scrolling state for a section,
  * based on which add or remove class (methods) for the navbar items
- * as well sections are called. 
+ * as well sections are called.
  */
-let activeScrollingCheck = ()=> {
+let activeScrollingCheck = () => {
+  listItemArray.forEach((link) => {
+    let section = document.querySelector(`#${link.dataset.flow}`);
 
-  listItemArray.forEach(link => {
-    let section = document.querySelector(link.hash);
+    const sectionOffset = Math.floor(section.getBoundingClientRect().top);
 
-    const sectionOffset = Math.floor(section.getBoundingClientRect().top)
+    removeActiveClass(link, section);
 
-
-    removeActiveClass(link,section);
-
-    addActiveClass((sectionOffset < 200 && sectionOffset >= -200),link,section);
-
-  })
+    addActiveClass(sectionOffset < 200 && sectionOffset >= -200, link, section);
+  });
 
   changeFloatingBtnVisibility();
-}
+};
 
 /**  method to change the visibility of the scroll to top button*/
-function changeFloatingBtnVisibility(){
-  let scrollHeight = document.documentElement.scrollTop || document.body.scrollTop;
+function changeFloatingBtnVisibility() {
+  let scrollHeight =
+    document.documentElement.scrollTop || document.body.scrollTop;
 
-  (scrollHeight > 500) ? topBtn.style.visibility = "visible" : topBtn.style.visibility = "hidden";
+  scrollHeight > 500
+    ? (topBtn.style.visibility = "visible")
+    : (topBtn.style.visibility = "hidden");
 }
 
-/** listening for a click on floating button, 
+/** listening for a click on floating button,
  *  scroll to top of the page */
-topBtn.addEventListener('click',()=>{window.scrollTo(0,0)})
+topBtn.addEventListener("click", () => {
+  window.scrollTo(0, 0);
+});
 
 /**  add class */
-function addActiveClass(visible,link,section){
-  if(visible){
+function addActiveClass(visible, link, section) {
+  if (visible) {
     link.classList.add("active");
-    section.classList.add('your-active-class');
+    section.classList.add("your-active-class");
   }
 }
 
 /** remove class */
-function removeActiveClass(link,section){
+function removeActiveClass(link, section) {
   link.classList.remove("active");
-  section.classList.remove('your-active-class');
+  section.classList.remove("your-active-class");
 }
 
 /** listen to the scrolling event */
-window.addEventListener('scroll', activeScrollingCheck )
-
+window.addEventListener("scroll", activeScrollingCheck);
 
 /** returns template string for section */
-function returnSectionString() { 
-  return `<section id="section${listItemArray.length+1}" data-nav="Section ${listItemArray.length+1}">
+function returnSectionString() {
+  return `<section id="section${listItemArray.length + 1}" data-nav="Section ${
+    listItemArray.length + 1
+  }">
   <div class="landing__container">
-    <h2>Section ${listItemArray.length+1}</h2>
+    <h2>Section ${listItemArray.length + 1}</h2>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum metus faucibus lectus pharetra dapibus. Suspendisse potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget lacinia ex. Phasellus imperdiet porta orci eget mollis. Sed convallis sollicitudin mauris ac tincidunt. Donec bibendum, nulla eget bibendum consectetur, sem nisi aliquam leo, ut pulvinar quam nunc eu augue. Pellentesque maximus imperdiet elit a pharetra. Duis lectus mi, aliquam in mi quis, aliquam porttitor lacus. Morbi a tincidunt felis. Sed leo nunc, pharetra et elementum non, faucibus vitae elit. Integer nec libero venenatis libero ultricies molestie semper in tellus. Sed congue et odio sed euismod.</p>
 
     <p>Aliquam a convallis justo. Vivamus venenatis, erat eget pulvinar gravida, ipsum lacus aliquet velit, vel luctus diam ipsum a diam. Cras eu tincidunt arcu, vitae rhoncus purus. Vestibulum fermentum consectetur porttitor. Suspendisse imperdiet porttitor tortor, eget elementum tortor mollis non.</p>
   </div>
-</section>`
+</section>`;
 }
-
-
